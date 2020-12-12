f444af11cc60db75416bc830a87bddafef10a798
alekgomes <lucas.maistro@gmail.com>
2020-12-12 18:49:51 -0300

    finalizando curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raat',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+
+  constructor() {
+  }  
 }
